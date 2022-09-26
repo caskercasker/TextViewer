7f11434356677208fc03b7bc96ae082fbcbebf5d
caskercasker <caskercasker@daum.net>
2022-09-26 18:42:33 +0900

    read txt file and portrait

--- TextViewer/ViewController.swift
+++ TextViewer/ViewController.swift
@@ -4,16 +4,63 @@
 //
 //  Created by 조우현 on 2022/09/26.
 //
-
+import Foundation
 import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {   //set delegate to class
+    
+    @IBOutlet weak var uiTitle: UILabel!
+    
+    @IBOutlet weak var TextField: UITextField! //textfield variable
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
+        var cc: Int = 1
+        var text: String
+//        text = """
+//        The powerful programming language that is also easy to learn.Swift is a powerfulnd intuitive programming language for iOS, iPadOS, macOS, tvOS, and watchOS. Writing Swift code is interactive and fun, the syntax is concise yet expressive, and Swift includes modern features developers love. Swift code is safe by design and produces software that runs lightning-fast.
+//        Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.
+//        """
+        //self.uiTitle.text = text
+        //self.textView.text = text
+        print("before")
+        text = readTextFile()
+        print("after")
+        print(text)
+        self.textView.text = text
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //TextField.delegate = self // set delegate
     }
-
-
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range:NSRange,replacementString string: String) -> Bool
+    {
+        let maxLength = 10
+        let currentString = (textField.text ?? "") as NSString
+        let newString = currentString.replacingCharacters(in: range, with: string)
+        
+        return newString.count <= maxLength
+    }
+    
+    func readTextFile() -> String {
+        var result:String = ""
+        let paths = Bundle.main.path(forResource: "sample.txt", ofType: nil)
+        guard paths != nil else {return "fatal"}
+        
+        do{
+            result = try String(contentsOfFile: paths!, encoding: .utf8)
+        }
+        catch let error as NSError{
+            print("open failure")
+            return "failure"
+        }
+        print("open success")
+        //print(result)
+        return result
+    }
+    
+    
 }
 
